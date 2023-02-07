01a457c14a49845fb0317abca68722a465c0d831
Pavel6516 <74011824+Pavel6516@users.noreply.github.com>
2023-02-07 16:37:06 +0500

    Методы и свойства

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-let title = 'Мой первый Js';
+let title;
+title = 'Мой первый Js';
 console.log(typeof title);
-let screens = 'Простые, сложные, интерактивные';
+
+let screens;
+screens = 'Простые, сложные, интерактивные';
 console.log(screens.toLowerCase().split());
-let screenPrice = 11125;
+
+let screenPrice;
+screenPrice = 11125;
 console.log('Стоимость верстки экранов', screenPrice, 'рублей.');
 
-let rollback = 74;
+let rollback;
+rollback = 74;
 console.log(rollback);
 
-let fullPrice = 2800000 ;
+let fullPrice;
+fullPrice = 2800000 ;
 console.log(typeof fullPrice);
 
-let adaptive = true;
+let adaptive;
+adaptive = true;
 console.log(typeof adaptive);
 
 console.log('Процент отката посреднику за работу',(fullPrice*(rollback/100)));
-// alert("It's me");
-// console.log("Первый вовод в консоль");
-// console.log("123");
+
+
 
