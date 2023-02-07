cc14c778aa3eea6548f4a5530de87a168e082058
Pavel6516 <74011824+Pavel6516@users.noreply.github.com>
2023-02-07 16:09:25 +0500

    Создание переменных

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-let title = 'Мой первый Js';
-console.log(typeof title);
-let screens = 'Простые, сложные, интерактивные';
-console.log(screens.toLowerCase().split());
-let screenPrice = 11125;
-console.log('Стоимость верстки экранов', screenPrice, 'рублей.');
-
-let rollback = 74;
-console.log(rollback);
-
-let fullPrice = 2800000 ;
-console.log(typeof fullPrice);
-
-let adaptive = true;
-console.log(typeof adaptive);
-
-console.log('Процент отката посреднику за работу',(fullPrice*(rollback/100)));
-// alert("It's me");
-// console.log("Первый вовод в консоль");
-// console.log("123");
-
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
