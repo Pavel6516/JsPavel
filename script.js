ac8cd6b1b1ed1bfe0dc30b9b447459c6aafd6e13
Pavel6516 <74011824+Pavel6516@users.noreply.github.com>
2023-02-07 16:33:46 +0500

    lesson02.2 итог

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-let title = 'Мой первый Js';
-let screens = 'Простые, сложные, интерактивные';
-let screenPrice = 11125;
-let rollback = 74;
-let fullPrice = 2800000 ;
-let adaptive = true;
+let title;
+title = 'Мой первый Js';
+console.log(typeof title);
+
+let screens;
+screens = 'Простые, сложные, интерактивные';
+console.log(screens.toLowerCase().split());
+
+let screenPrice;
+screenPrice = 11125;
+console.log('Стоимость верстки экранов', screenPrice, 'рублей.');
+
+let rollback;
+rollback = 74;
+console.log(rollback);
+
+let fullPrice;
+fullPrice = 2800000 ;
+console.log(typeof fullPrice);
+
+let adaptive;
+adaptive = true;
+console.log(typeof adaptive);
+
+console.log('Процент отката посреднику за работу',(fullPrice*(rollback/100)));
+
+
