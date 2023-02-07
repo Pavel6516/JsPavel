9de81417a1c8765c3727f0a576a77417165363a3
Pavel6516 <74011824+Pavel6516@users.noreply.github.com>
2023-02-07 16:40:49 +0500

    Присвоение переменным значения

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
 let title;
 title = 'Мой первый Js';
-console.log(typeof title);
 
 let screens;
 screens = 'Простые, сложные, интерактивные';
-console.log(screens.toLowerCase().split());
 
 let screenPrice;
 screenPrice = 11125;
-console.log('Стоимость верстки экранов', screenPrice, 'рублей.');
 
 let rollback;
 rollback = 74;
-console.log(rollback);
 
 let fullPrice;
 fullPrice = 2800000 ;
-console.log(typeof fullPrice);
 
 let adaptive;
 adaptive = true;
-console.log(typeof adaptive);
 
-console.log('Процент отката посреднику за работу',(fullPrice*(rollback/100)));
 
 
